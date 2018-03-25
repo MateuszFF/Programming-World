ed8d45cfa8c03df2b7694f8f1b865cc33a0a6837
Mateusz <37412088+MateuszFF@users.noreply.github.com>
2018-03-25 13:44:40 +0200

    Create else if.java

---
+++ else if.java
@@ -0,0 +1,21 @@
+public class Main 
+{
+    public static void main(String args[])
+    {
+
+        int a = 9;
+        if (a<10)
+            System.out.println("a<10");
+        if(a==10)
+            System.out.println("a=10");
+        if(a==9)
+            System.out.println("a=9");
+        if(a==25)
+            System.out.println("a=25");
+        else
+            System.out.println("a>10");
+
+    }
+
+
+}
