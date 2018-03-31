19ed32993db02381ca4b1931537e61bc11995285
Mateusz <37412088+MateuszFF@users.noreply.github.com>
2018-03-31 15:01:50 +0200

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,24 @@
+package com.navin;
+
+import java.net.SocketPermission;
+import java.util.Scanner;
+
+public class Fibonacci
+{
+
+    public static void main(String[] args)
+    {
+        int k,a=1,b=1;
+        Scanner sc= new Scanner(System.in);
+        k=0;
+        System.out.println("Enter any number");
+        int n = sc.nextInt();
+        System.out.print("1 1");
+        while (k<=n)
+        {
+            k=a+b;
+            System.out.print(k + " ");
+            a=b;
+        }
+
+    }
